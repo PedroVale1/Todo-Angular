cac4d3e8eee25def0a2231d71392e6b2cdf49049
PedroVale1 <162756423+PedroVale1@users.noreply.github.com>
2025-02-02 01:09:17 -0300

    Add files via upload
    faltando apenas agora o css para style da pagina, fucionalidades tudo ok e testada salvando e removendo do local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Todo } from 'src/models/todo.model';
 
 @Component({
@@ -7,13 +8,31 @@ import { Todo } from 'src/models/todo.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public todos: Todo[] = []
+  public todos: Todo[] = [];
   public title: String = 'Minhas tarefas';
+  public form: FormGroup;
 
-  constructor() {
-    this.todos.push(new Todo(1, 'passear com cachorro' , false));
-    this.todos.push(new Todo(2, 'ir ao supermercado' , false));
-    this.todos.push(new Todo(3, 'cortar o cabelo' , true));
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['',Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    });
+
+    this.load();
+  }
+  add(){
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length +1;
+    this.todos.push(new Todo(id, title, false));
+    this.save();
+    this.clear();
+  }
+
+  clear(){
+    this.form.reset();
   }
 
   remove(todo: Todo){
@@ -21,11 +40,24 @@ export class AppComponent {
     if (index != -1 ){
       this.todos.splice(index, 1);
     }
+    this.save();
   }
-  markAsDone(todo){
+  markAsDone(todo: Todo){
     todo.done = true;
+    this.save();
   }
   markUsDone(todo: Todo){
-    todo.done = false
+    todo.done = false;
+    this.save();
+  }
+
+  save(){
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+  }
+
+  load(){
+    const data = localStorage.getItem('todos');
+    this.todos = data? JSON.parse(data): [];
   }
 }
